d3aff5c947b2c09e3391ad9520c53f291ff0a7e0
aulaleslie <aulaleslie@gmail.com>
2023-08-07 17:48:07 +0800

    feat: add cmd app

--- main.go
+++ main.go
@@ -4,38 +4,21 @@ import (
 	"learn-cli/app"
 	"learn-cli/atm_service"
 	"learn-cli/db"
+	"learn-cli/helper"
 )
 
 func main() {
-	file := db.NewFileDB()
+	file := db.NewFileDB("users.json")
 
 	err := file.Open()
-	panicIfError(err)
+	helper.PanicIfError(err)
 
 	atmService := atm_service.NewService()
 
-	cmd := app.NewApp(atmService, db.GetUsers())
+	cmd := app.NewApp(atmService)
 	err = cmd.Run()
-	panicIfError(err)
+	helper.PanicIfError(err)
 
 	_ = file.Close()
-	panicIfError(err)
-}
-
-func panicIfError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func atm() {
-	// read file
-
-	// parsing json store ke variable
-
-	// baca argumen
-
-	// proces input
-
-	// close file
+	helper.PanicIfError(err)
 }
